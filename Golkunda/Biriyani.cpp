22daedb6b57fe558993c6d3dabb624d0d5636b7f
Sudipto Mandal <sudipto.vz@gmail.com>
2015-08-22 12:11:52 +0530

    ISSUE#0002: Adding Biriyani and Kebab Info. 	Let's eat now.

---
+++ Golkunda/Biriyani.cpp
@@ -0,0 +1,5 @@
+Best Biriyani in Hyderabad
+1. Paradise
+2. Hyderabad House
+3. Four Square
+4. Bawarchi
---
+++ Golkunda/Kabab.cs
@@ -0,0 +1,7 @@
+include System;
+include System.Text;
+
+public class Kabab
+{
+	Console.WriteLine("Lets eat Kabab");
+}
